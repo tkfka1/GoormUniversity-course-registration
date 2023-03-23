b82fb65d6a61a7e850168f6cf9cb226514b99db2
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-03-23 21:27:06 +0900

    test

---
+++ backend/back/src/main/java/com/hodolog/api/domain/Session.java
@@ -0,0 +1,36 @@
+package com.hodolog.api.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import java.util.UUID;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String accessToken;
+
+    @ManyToOne
+    private User user;
+
+    @Builder
+    public Session(User user) {
+        this.accessToken = UUID.randomUUID().toString();
+        this.user = user;
+    }
+}
---
+++ backend/back/src/main/java/com/hodolog/api/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.hodolog.api.repository;
+
+import com.hodolog.api.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmailAndPassword(String email, String password);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByStudentid(String studentid);
+}
